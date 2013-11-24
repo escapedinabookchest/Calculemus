f18af724dff0d6bcfd80556759da49c55265fdff
nope <escapedinabookchest@gmail.com>
2013-11-24 16:23:27 +0100

    Parser
    I've added a working parser and unit tests of the parser.

---
+++ Calculemus/Parser.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Calculemus
+{
+    /**
+     * The class Parser parses the textfile and puts its vertices and edges in the appointed dictionaries. 
+     */
+    public class Parser {
+        private Dictionary<string, string> vertices;
+        private Dictionary<string, LinkedList<string>> edges;
+
+        public Parser()
+        {
+            vertices = new Dictionary<string, string>();
+            edges = new Dictionary<string, LinkedList<string>>();
+        }
+
+        public Dictionary<string, string> Vertices
+        {
+            get { return this.vertices; }
+            set { this.vertices = value; }
+        }
+
+        public Dictionary<string, LinkedList<string>> Edges
+        {
+            get { return this.edges; }
+            set { this.edges = value; }
+        }
+
+        public void Parse(string filename)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filename);
+                int errors = 0;
+
+                foreach (string line in lines)
+                {
+                    errors = ParseLine(line) ? errors : errors++;
+                }
+
+                if (errors > 0)
+                    Console.WriteLine("There are {0} syntax errors in file {1}!", errors, filename);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private Boolean ParseLine(string line)
+        {
+            if (IsVertex(line))
+            {
+                AddVertex(line);
+                return true;
+            }
+
+            if (IsEdge(line))
+            {
+                AddEdge(line);
+                return true;
+            }
+
+            if (IsComment(line))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void AddVertex(string line)
+        {
+            StringBuilder sb = new StringBuilder();
+            string key = "";
+            string value = "";
+
+            foreach (char c in line)
+            {
+                switch (c)
+                {
+                    case '*':
+                        break;
+
+                    case ';':
+                        value = sb.ToString();
+                        sb.Clear();
+                        break;
+
+                    case ':':
+                        key = sb.ToString();
+                        sb.Clear();
+                        break;
+
+                    default:
+                        sb.Append(c);
+                        break;
+                }
+            }
+
+            Vertices.Add(key, value);
+        }
+
+        private void AddEdge(string line)
+        {
+            StringBuilder sb = new StringBuilder();
+            string key = "";
+            LinkedList<string> values = new LinkedList<string>();
+
+            foreach (char c in line)
+            {
+                switch (c)
+                {
+                    case '!':
+                        break;
+
+                    case ';':
+                        values.AddFirst(sb.ToString());
+                        sb.Clear();
+                        break;
+
+                    case ',':
+                        values.AddFirst(sb.ToString());
+                        sb.Clear();
+                        break;
+
+                    case ':':
+                        key = sb.ToString();
+                        sb.Clear();
+                        break;
+
+                    default:
+                        sb.Append(c);
+                        break;
+                }
+            }
+
+            Edges.Add(key, values);
+        }
+
+        private Boolean IsVertex(string line)
+        {
+            Match match = Regex.Match(line, @"^[*][A-Z]{1,8}([0-9]{1,4})?[:][A-Z_]{1,20}[;]$");
+
+            if (match.Success)
+                return true;
+
+            return false;
+        }
+
+        private Boolean IsEdge(string line)
+        {
+            Match match = Regex.Match(line, @"^[!][A-Z]{1,8}([0-9]{1,4})?[:]([A-Z]{1,8}([0-9]{1,4})?[,]?)*[;]$");
+
+            if (match.Success)
+                return true;
+
+            return false;
+        }
+
+        private Boolean IsComment(string line)
+        {
+            Match match = Regex.Match(line, @"^#");
+
+            if (match.Success)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ TestCalculemus/TestParser.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculemus;
+using System.Collections.Generic;
+
+namespace TestCalculemus
+{
+    [TestClass]
+    public class TestParser
+    {
+        private Parser parser;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            parser = new Parser();
+        }
+
+        [TestMethod]
+        public void TestParse()
+        {
+            parser.Parse(@"C:\Users\Hersenspinsel\Documents\GitHub\Calculemus\Calculemus\circuit.txt");
+
+            Console.WriteLine("Vertices:");
+            foreach (KeyValuePair<string, string> pair in parser.Vertices)
+            {
+                Console.WriteLine(pair.Key + " " + pair.Value);
+            }
+
+            Console.WriteLine("Edges:");
+            foreach (KeyValuePair<string, LinkedList<string>> pair in parser.Edges)
+            {
+                Console.Write(pair.Key + ": ");
+
+                foreach (string value in pair.Value)
+                {
+                    Console.Write(value + ",");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        //[TestMethod]
+        //public void TestAddVertex()
+        //{
+        //    Dictionary<string, string> vertices = new Dictionary<string, string>();
+        //    vertices.Add("NODE1", "AND");
+
+        //    parser.AddVertex("NODE1:AND;");
+
+        //    CollectionAssert.AreEqual(vertices, parser.Vertices);
+        //}
+
+        //[TestMethod]
+        //public void TestAddEdge()
+        //{
+        //    Dictionary<string, LinkedList<string>> edges = new Dictionary<string, LinkedList<string>>();
+
+        //    LinkedList<string> values = new LinkedList<string>();
+        //    values.AddFirst("NODE1");
+        //    values.AddFirst("NODE2");
+        //    values.AddFirst("NODE3");
+        //    edges.Add("A", values);
+
+        //    parser.AddEdge("!A:NODE1,NODE2,NODE3;");
+
+        //    foreach (KeyValuePair<string, LinkedList<string>> pair in parser.Edges)
+        //    {
+        //        Console.WriteLine(pair.Key);
+
+        //        foreach(string value in pair.Value)
+        //        {
+        //            Console.WriteLine(value);
+        //        }
+        //    }
+
+        //    CollectionAssert.AreEqual(edges, parser.Edges);
+        //}
+
+        //[TestMethod]
+        //public void TestIsVertex()
+        //{
+        //    Assert.IsTrue(parser.IsVertex("*A:INPUT_HIGH;"));
+        //}
+
+        //[TestMethod]
+        //public void TestIsEdge()
+        //{
+        //    Assert.IsTrue(parser.IsEdge("!A:NODE1,NODE2,NODE3;"));
+        //}
+
+        //[TestMethod]
+        //public void TestIsComment()
+        //{
+        //    Assert.IsTrue(parser.IsComment("# This is a comment."));
+        //}
+    }
+}
